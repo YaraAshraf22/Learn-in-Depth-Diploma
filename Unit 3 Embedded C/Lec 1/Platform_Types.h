2b803982eb7ac76be6603844b75afde8edd59a0c
Yara Ashraf <yaraashraf5799@gmail.com>
2024-01-07 14:12:06 +0200

    Adding Platform_Types header

---
+++ Unit 3 Embedded C/Lec 1/Platform_Types.h
@@ -0,0 +1,37 @@
+/*
+ * Platform_Types.h
+ *
+ *  Created on: Jan 7, 2024
+ *      Author: HP
+ */
+
+#ifndef PLATFORM_TYPES_H_
+#define PLATFORM_TYPES_H_
+
+
+typedef  signed char					sint8_t;
+typedef  unsigned char					uint8_t;
+
+typedef  signed short					sint16_t;
+typedef  unsigned short					uint16_t;
+
+typedef  signed int						sint32_t;
+typedef  unsigned int					uint32_t;
+
+typedef  signed long long				sint64_t;
+typedef  unsigned long long				uint64_t;
+
+typedef  volatile signed char			vint8_t;
+typedef  volatile unsigned char			vuint8_t;
+
+typedef  volatile signed short			vint16_t;
+typedef  volatile unsigned short		vuint16_t;
+
+typedef  volatile signed int			vint32_t;
+typedef  volatile unsigned int			vuint32_t;
+
+typedef  volatile signed long long		vint64_t;
+typedef  volatile unsigned long long	vuint64_t;
+
+
+#endif /* PLATFORM_TYPES_H_ */
